1375cd5ffe46860f8a437b9a9598b9fd60739fd2
Zachary Fogg <me@zfo.gg>
2023-02-20 21:19:01 -0500

    deployed baoETH. will update with instructions in the future...

--- deploy/04_01_baoETH.js
+++ deploy/04_01_baoETH.js
@@ -1,11 +1,11 @@
-const SAFE = '0xFC69e0a5823E2AfCBEb8a35d33588360F1496a00'
-//const SAFE = '0x3dFc49e5112005179Da613BdE5973229082dAc35'
-const FED_ADMIN = '0xFC69e0a5823E2AfCBEb8a35d33588360F1496a00'
-const MINT_LIMIT = "1000000000000000000000"
+const SAFE           = '0xFC69e0a5823E2AfCBEb8a35d33588360F1496a00'
+const MINT_LIMIT     = "1000000000000000000000"
 const BALLAST_SUPPLY = "200000000000000000000"
 
 module.exports = async ({getNamedAccounts, deployments, ethers}) => {
   const { deployer } = await getNamedAccounts()
+  const p = ethers.getDefaultProvider()
+  console.log("balance", ethers.utils.formatEther(await p.getBalance(deployer)))
 
   let tx
 
@@ -29,13 +29,13 @@ module.exports = async ({getNamedAccounts, deployments, ethers}) => {
   const unitroller = new ethers.Contract(unitrollerDeploy.address, unitrollerDeploy.abi, await ethers.getSigner(deployer))
 
 
-  console.log("Becoming the unitroller as the comptroller...")
-  tx = await unitroller._setPendingImplementation(comptroller.address)
-  await tx.wait()
-  tx = await comptroller._become(unitroller.address)
-  await tx.wait()
-  console.log("... became!")
-  comptroller = new ethers.Contract(unitrollerDeploy.address, comptrollerDeploy.abi, await ethers.getSigner(deployer))
+  //console.log("Becoming the unitroller as the comptroller...")
+  //tx = await unitroller._setPendingImplementation(comptroller.address)
+  //await tx.wait()
+  //tx = await comptroller._become(unitroller.address)
+  //await tx.wait()
+  //console.log("... became!")
+  //comptroller = new ethers.Contract(unitrollerDeploy.address, comptrollerDeploy.abi, await ethers.getSigner(deployer))
 
 
   const oracleDeploy = await deployments.deploy('oracle-baoETH', {
@@ -56,15 +56,35 @@ module.exports = async ({getNamedAccounts, deployments, ethers}) => {
   })
   const baoETH = new ethers.Contract(baoETHDeploy.address, baoETHDeploy.abi, await ethers.getSigner(deployer))
 
-
   //console.log("Setting baoETH's admin to be the DAO treasury...")
   //tx = await baoETH.setPendingOperator(SAFE)
   //await tx.wait()
   //console.log("... set! Now the treasury must call claimOperator().")
 
-  // TODO:
-  // check interest rate model numbers
-  // check initial numbers for setting up market (collateral factor, IMF factor)
+
+  const jumpRateModelDeploy = await deployments.deploy('JumpRateModelV2', {
+    from: deployer,
+    contract: 'JumpRateModelV2',
+    args: [
+      "0", //uint baseRatePerYear
+      "49999999998268800", //uint multiplierPerYear
+      "1089999999998841600", //uint jumpMultiplierPerYear
+      "800000000000000000", //uint kink_
+    ],
+    log: true,
+  })
+  const jumpRateModel = new ethers.Contract(jumpRateModelDeploy.address, jumpRateModelDeploy.abi, await ethers.getSigner(deployer))
+
+  const whitePaperModelDeploy = await deployments.deploy('WhitePaperInterestRateModel', {
+    from: deployer,
+    contract: 'WhitePaperInterestRateModel',
+    args: [
+      "19999999999728000", //uint baseRatePerYear
+      "99999999998640000", //uint multiplierPerYear
+    ],
+    log: true,
+  })
+  const whitePaperModel = new ethers.Contract(whitePaperModelDeploy.address, whitePaperModelDeploy.abi, await ethers.getSigner(deployer))
 
 
   // bao deposited baoETH - bdbaoETH
@@ -74,7 +94,7 @@ module.exports = async ({getNamedAccounts, deployments, ethers}) => {
     args: [
       baoETHDeploy.address,                         // underlying
       unitrollerDeploy.address,                     // unitroller
-      '0x681Cf55f0276126FAD8842133C839AB4D607E729', // interest rate model (JumpRateModelV2)
+      jumpRateModelDeploy.address,                  // interest rate model (JumpRateModelV2)
       '200000000000000000',                         // initial exchange rate mantissa
       'bao deposited baoETH',                       // name
       'bdbaoETH',                                   // symbol
@@ -92,7 +112,7 @@ module.exports = async ({getNamedAccounts, deployments, ethers}) => {
     contract: 'CEther',
     args: [
       unitrollerDeploy.address,                     // unitroller
-      '0xfbc3995AADfAf39896CEAE3d8aAa3AFd1dE261Ad', // interest rate model (WhitePaperInterestRateModel)
+      whitePaperModelDeploy.address,                // interest rate model (WhitePaperInterestRateModel)
       '200000000000000000',                         // initial exchange rate mantissa
       'bao deposited Ether',                        // name
       'bdETH',                                      // symbol
@@ -109,7 +129,7 @@ module.exports = async ({getNamedAccounts, deployments, ethers}) => {
     args: [
       '0xa1e3F062CE5825c1e19207cd93CEFdaD82A8A631', // underlying
       unitrollerDeploy.address,                     // unitroller
-      '0xfbc3995AADfAf39896CEAE3d8aAa3AFd1dE261Ad', // interest rate model (WhitePaperInterestRateModel)
+      whitePaperModelDeploy.address,                // interest rate model (WhitePaperInterestRateModel)
       '200000000000000000',                         // initial exchange rate mantissa
       'bao deposited bETH',                         // name
       'bdbETH',                                     // symbol
@@ -128,7 +148,7 @@ module.exports = async ({getNamedAccounts, deployments, ethers}) => {
     args: [
       '0x5ee08f40b637417bcC9d2C51B62F4820ec9cF5D8', // underlying
       unitrollerDeploy.address,                     // unitroller
-      '0x681Cf55f0276126FAD8842133C839AB4D607E729', // interest rate model (WhitePaperInterestRateModel)
+      jumpRateModelDeploy.address,                  // interest rate model (WhitePaperInterestRateModel)
       '200000000000000000',                         // initial exchange rate mantissa
       'bao deposited bSTBL',                        // name
       'bdbSTBL',                                    // symbol
@@ -141,87 +161,97 @@ module.exports = async ({getNamedAccounts, deployments, ethers}) => {
   const bdbSTBL = new ethers.Contract(bdbSTBLDeploy.address, bdbSTBLDeploy.abi, await ethers.getSigner(deployer))
 
 
-  console.log("Setting up price oracle...")
-  tx = await oracle.setFeed(bdbaoETH.address, "0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419", "18");
-  await tx.wait();
-  tx = await oracle.setFeed(bdEther.address, "0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419", "18");
-  await tx.wait();
-  tx = await oracle.setFeed(bdbSTBL.address, "0x4626CEB86aBF6B239e3306382Cca44e5B7113160", "18");
-  await tx.wait();
+  //console.log("Setting up price oracle...")
+  //tx = await oracle.setFeed(bdbaoETH.address, "0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419", "18");
+  //await tx.wait();
+  //tx = await oracle.setFeed(bdEther.address, "0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419", "18");
+  //await tx.wait();
+  //tx = await oracle.setFeed(bdbSTBL.address, "0x4626CEB86aBF6B239e3306382Cca44e5B7113160", "18");
+  //await tx.wait();
 
 
-  console.log("Setting up baoETH market with collateral of [ETH, bETH, bSTBL]...")
-  tx = await comptroller._setPriceOracle(oracle.address)
-  await tx.wait()
-  tx = await comptroller._setCloseFactor("500000000000000000")
-  await tx.wait()
-  tx = await comptroller._setLiquidationIncentive("1100000000000000000")
-  await tx.wait()
-  tx = await comptroller._supportMarket(bdbaoETH.address)
-  await tx.wait()
-  tx = await comptroller._supportMarket(bdEther.address)
-  await tx.wait()
-  tx = await comptroller._supportMarket(bdbETH.address)
-  await tx.wait()
-  tx = await comptroller._supportMarket(bdbSTBL.address)
-  await tx.wait()
-  tx = await comptroller._setCollateralFactor(bdbaoETH.address, "250000000000000000")
-  await tx.wait()
-  tx = await comptroller._setCollateralFactor(bdEther.address, "900000000000000000")
-  await tx.wait()
-  tx = await comptroller._setCollateralFactor(bdbETH.address, "900000000000000000")
-  await tx.wait()
-  tx = await comptroller._setCollateralFactor(bdbSTBL.address, "750000000000000000")
-  await tx.wait()
-  tx = await comptroller._setIMFFactor(bdbaoETH.address, "40000000000000000")
-  await tx.wait()
-  tx = await comptroller._setIMFFactor(bdEther.address, "40000000000000000")
-  await tx.wait()
-  tx = await comptroller._setIMFFactor(bdbETH.address, "40000000000000000")
-  await tx.wait()
-  tx = await comptroller._setIMFFactor(bdbSTBL.address, "40000000000000000")
-  await tx.wait()
-  tx = await comptroller._setMarketBorrowCaps([bdbaoETH.address], [MINT_LIMIT])
-  await tx.wait()
-  tx = await comptroller._setBorrowRestriction([bdbaoETH.address], [false])
-  await tx.wait()
-  console.log("... supported!")
+  //console.log("Setting up baoETH market with collateral of [ETH, bETH, bSTBL]...")
+  //tx = await comptroller._setPriceOracle(oracle.address)
+  //await tx.wait()
+  //tx = await comptroller._setCloseFactor("500000000000000000")
+  //await tx.wait()
+  //tx = await comptroller._setLiquidationIncentive("1100000000000000000")
+  //await tx.wait()
+  //tx = await comptroller._supportMarket(bdbaoETH.address)
+  //await tx.wait()
+  //tx = await comptroller._supportMarket(bdEther.address)
+  //await tx.wait()
+  //tx = await comptroller._supportMarket(bdbETH.address)
+  //await tx.wait()
+  //tx = await comptroller._supportMarket(bdbSTBL.address)
+  //await tx.wait()
+  //tx = await comptroller._setCollateralFactor(bdbaoETH.address, "250000000000000000")
+  //await tx.wait()
+  //tx = await comptroller._setCollateralFactor(bdEther.address, "900000000000000000")
+  //await tx.wait()
+  //tx = await comptroller._setCollateralFactor(bdbETH.address, "900000000000000000")
+  //await tx.wait()
+  //tx = await comptroller._setCollateralFactor(bdbSTBL.address, "750000000000000000")
+  //await tx.wait()
+  //tx = await comptroller._setIMFFactor(bdbaoETH.address, "40000000000000000")
+  //await tx.wait()
+  //tx = await comptroller._setIMFFactor(bdEther.address, "40000000000000000")
+  //await tx.wait()
+  //tx = await comptroller._setIMFFactor(bdbETH.address, "40000000000000000")
+  //await tx.wait()
+  //tx = await comptroller._setIMFFactor(bdbSTBL.address, "40000000000000000")
+  //await tx.wait()
+  //tx = await comptroller._setMarketBorrowCaps([bdbaoETH.address], [MINT_LIMIT])
+  //await tx.wait()
+  //tx = await comptroller._setBorrowRestriction([bdbaoETH.address], [false])
+  //await tx.wait()
+  //console.log("... supported!")
 
-  console.log("Setting reserve factors...")
-  tx = await bdbaoETH._setReserveFactor("500000000000000000")
-  await tx.wait()
-  tx = await bdEther._setReserveFactor("500000000000000000")
-  await tx.wait()
-  tx = await bdbETH._setReserveFactor("500000000000000000")
-  await tx.wait()
-  tx = await bdbSTBL._setReserveFactor("500000000000000000")
-  await tx.wait()
-  console.log("... set!")
+  //console.log("Setting unitroller admin...")
+  //tx = await unitroller._setPendingAdmin(SAFE)
+  //await tx.wait()
+  //console.log("... set!")
 
-  //console.log("Setting bdbaoETH's admin to be the DAO treasury...")
+  //console.log("Setting reserve factors...")
+  //tx = await bdbaoETH._setReserveFactor("500000000000000000")
+  //await tx.wait()
+  //tx = await bdEther._setReserveFactor("500000000000000000")
+  //await tx.wait()
+  //tx = await bdbETH._setReserveFactor("500000000000000000")
+  //await tx.wait()
+  //tx = await bdbSTBL._setReserveFactor("500000000000000000")
+  //await tx.wait()
+  //console.log("... set!")
+
+  //console.log("Setting the bdTokens' admin to be the DAO treasury...")
   //tx = await bdbaoETH._setPendingAdmin(SAFE)
+  //await tx.wait()
+  //tx = await bdEther._setPendingAdmin(SAFE)
+  //await tx.wait()
+  //tx = await bdbETH._setPendingAdmin(SAFE)
+  //await tx.wait()
+  //tx = await bdbSTBL._setPendingAdmin(SAFE)
   //await tx.wait()
   //console.log("... set! Now the treasury must call _acceptAdmin().")
 
 
   const fedDeploy = await deployments.deploy('Fed-bdbaoETH', {
     from: deployer,
-    contract: 'contracts/InverseFinance/Fed.sol:Fed',
-    args: [bdbaoETHDeploy.address],
+    contract: 'contracts/markets/Fed2.sol:Fed',
+    args: [bdbaoETHDeploy.address, "0x0000000000000000000000000000000000000000"],
     log: true,
   })
   const fed = new ethers.Contract(fedDeploy.address, fedDeploy.abi, await ethers.getSigner(deployer))
 
-  console.log("Adding the fed as a minter...")
-  tx = await baoETH.addMinter(fedDeploy.address)
-  await tx.wait()
-  console.log("... added!")
+  //console.log("Adding the fed as a minter...")
+  //tx = await baoETH.addMinter(fedDeploy.address)
+  //await tx.wait()
+  //console.log("... added!")
 
-  //TODO: needs the comptroller to start supporting the market first
-  console.log("Expanding the fed")
-  tx = await fed.expansion(MINT_LIMIT)
-  await tx.wait()
-  console.log("... expanded!")
+  //console.log("Expanding the fed")
+  //tx = await fed.expansion(MINT_LIMIT)
+  //await tx.wait()
+  //console.log("... expanded!")
 
   console.log("Accruing interest...")
   tx = await bdbaoETH.accrueInterest()
@@ -230,13 +260,12 @@ module.exports = async ({getNamedAccounts, deployments, ethers}) => {
   await tx.wait()
   console.log("... accrued!")
 
-  //console.log("Setting fed admin...")
-  //tx = await fed.changeGov(FED_ADMIN)
-  //await tx.wait()
-  //tx = await fed.changeChair(FED_ADMIN)
-  //await tx.wait()
-  //console.log("... set!")
-
+  console.log("Setting fed admin...")
+  tx = await fed.changeChair(SAFE)
+  await tx.wait()
+  tx = await fed.changeGov(SAFE)
+  await tx.wait()
+  console.log("... set!")
 
   const ballastDeploy = await deployments.deploy('Ballast-baoETH', {
     from: deployer,
@@ -263,6 +292,8 @@ module.exports = async ({getNamedAccounts, deployments, ethers}) => {
     { name: 'bdbETH'     , address: bdbETHDeploy      .address },
     { name: 'bdbSTBL'    , address: bdbSTBLDeploy     .address },
   ])
+
+  console.log("balance", await p.getBalance(deployer))
 }
 
 module.exports.tags = ['Phase4', 'baoETH']
