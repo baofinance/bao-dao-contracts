c0cc3dc933a2a5a4b7010388bc96256c435e4013
Zachary Fogg <me@zfo.gg>
2023-05-08 16:36:14 -0400

    added a suggestion and abstracted the contract more

--- contracts/markets/FeeDistributorProxy.sol
+++ contracts/markets/FeeDistributorProxy.sol
@@ -7,19 +7,21 @@ contract FeeDistributorProxy {
     using SafeMath for uint;
 
     address public admin;
+    address public operator;
 
     uint public x;
     uint public y;
     uint public z;
 
-    ERC20 public baoUSD = ERC20(0x7945b0A6674b175695e5d1D08aE1e6F13744Abb0);
+    ERC20 public token;
 
     address public outputX = 0x3dCe48CfC0bEA704ec1640b34b33eC55F97D3056;
     address public outputY = 0x00000000000000000000000000000000DeaDBeef;
     address public outputZ = 0x00000000000000000000000000000000cafebabe;
 
-    constructor(address _admin, uint _x, uint _y, uint _z) public {
+    constructor(address _admin, ERC20 _token, uint _x, uint _y, uint _z) public {
         admin = _admin;
+        token = _token;
         x = _x;
         y = _y;
         z = _z;
@@ -29,10 +31,21 @@ contract FeeDistributorProxy {
     function _onlyAdmin() private {
         require(msg.sender == admin, "only admin");
     }
+
+    function _onlyOperator() private {
+        require(msg.sender == operator, "only operator");
+    }
+
     function _validPercent(uint _p) private {
         require(_p <= 1000000000000000000, "percent too big");
     }
     // }}}
+
+    // Set the address that can operate some of this function (less than admin).
+    function setOperator(address _operator) external {
+        _onlyAdmin();
+        operator = _operator;
+    }
 
     // Withdraw any amount of a token held by this contract to some address.
     function withdraw(ERC20 _token, address _output, uint _amount) public {
@@ -73,22 +86,22 @@ contract FeeDistributorProxy {
     // Split a set amount of the held funds and send it.
     // x+y+z have to add up to "one" (1e18) before you call this.
     function splitFunds(uint _amount) public {
-        _onlyAdmin();
+        _onlyOperator();
         require(x+y+z == 1000000000000000000, "x+y+z != 100%");
-        require(_amount <= baoUSD.balanceOf(address(this)), "not enough funds");
+        require(_amount <= token.balanceOf(address(this)), "not enough funds");
 
         uint amountX = (_amount * x) / 10**18;
         uint amountY = (_amount * y) / 10**18;
         uint amountZ = (_amount * z) / 10**18;
 
-        baoUSD.transfer(outputX, amountX);
-        baoUSD.transfer(outputY, amountY);
-        baoUSD.transfer(outputZ, amountZ);
+        token.transfer(outputX, amountX);
+        token.transfer(outputY, amountY);
+        token.transfer(outputZ, amountZ);
     }
 
     // Split the entire amount of held funds up.
     function splitFunds() external {
-        uint fullBalance = baoUSD.balanceOf(address(this));
+        uint fullBalance = token.balanceOf(address(this));
         splitFunds(fullBalance);
     }
 }
