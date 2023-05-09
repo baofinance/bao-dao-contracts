fd9d6ec4d51312d4884146df5792afcc6ce016db
Haruxe <haruxe@proton.me>
2023-05-09 11:49:08 -0700

    revise FeeDistributorProxy.sol

--- contracts/markets/FeeDistributorProxy.sol
+++ contracts/markets/FeeDistributorProxy.sol
@@ -1,107 +1,164 @@
+// SPDX-License-Identifier: MIT
+
 import "./SafeMath.sol";
-import "./ERC20.sol";
+import "./IERC20.sol";
 
-pragma solidity ^0.5.16;
+pragma solidity 0.8.19;
 
-contract FeeDistributorProxy {
-    using SafeMath for uint;
+error InvalidPercentages();
+error AddressZero();
+error NotEnoughFunds();
 
-    address public admin;
-    address public operator;
+contract FeeDistributorPropercentXpercentY {
 
-    uint public x;
-    uint public y;
-    uint public z;
+    address public admin; // Address of the admin
+    address public operator; // Address of the operator
 
-    ERC20 public token;
+    uint64 public percentX; // Percentage of funds to distribute to address X
+    uint64 public percentY; // Percentage of funds to distribute to address Y
+    uint64 public percentZ; // Percentage of funds to distribute to address Z
 
-    address public outputX = 0x3dCe48CfC0bEA704ec1640b34b33eC55F97D3056;
-    address public outputY = 0x00000000000000000000000000000000DeaDBeef;
-    address public outputZ = 0x00000000000000000000000000000000cafebabe;
+    IERC20 public token; // The token to be distributed
 
-    constructor(address _admin, ERC20 _token, uint _x, uint _y, uint _z) public {
+    address public immutable recipientX; // Address of recipient X
+    address public immutable recipientY; // Address of recipient Y
+    address public immutable recipientZ; // Address of recipient Z  
+    
+
+    /// @param _admin The admin address
+    /// @param _operator The operator address
+    /// @param _token The token to be distributed
+    /// @param _percentX The percentage of funds to distribute to address X
+    /// @param _percentY The percentage of funds to distribute to address Y
+    /// @param _percentZ The percentage of funds to distribute to address Z
+    constructor(address _admin, address _operator, IERC20 _token, uint _percentX, uint _percentY, uint _percentZ) public {
+        // ensure percentages do not exceed 100%
+        if ((_percentX + _percentY + _percentZ) == 1_000_000_000_000_000_000) {
+            revert InvalidPercentages();
+        }
+
+        if (admin == address(0) || address(_token) == address(0) || _operator == address(0)){
+            revert AddressZero();
+        }
+
         admin = _admin;
         token = _token;
-        x = _x;
-        y = _y;
-        z = _z;
-    }
-
-    // Assertions {{{
-    function _onlyAdmin() private {
-        require(msg.sender == admin, "only admin");
-    }
-
-    function _onlyOperator() private {
-        require(msg.sender == operator, "only operator");
-    }
-
-    function _validPercent(uint _p) private {
-        require(_p <= 1000000000000000000, "percent too big");
-    }
-    // }}}
-
-    // Set the address that can operate some of this function (less than admin).
-    function setOperator(address _operator) external {
-        _onlyAdmin();
         operator = _operator;
+
+        percentX = _percentX;
+        percentY = _percentY;
+        percentZ = _percentZ;
     }
 
-    // Withdraw any amount of a token held by this contract to some address.
-    function withdraw(ERC20 _token, address _output, uint _amount) public {
-        _onlyAdmin();
+    /**
+    * @dev Require that the caller is the admin.
+    */
+    modifier onlyAdmin(){
+        require(msg.sender == admin, "only admin");
+        _;
+    }
+
+    /**
+    * @dev Require that the caller is the operator.
+    */
+    modifier onlyOperator(){
+        require(msg.sender == operator, "only operator");
+        _;
+    }
+
+    /**
+    * @dev Withdraws an amount of a token held by this contract to a specified address.
+    * @param _token The token to withdraw.
+    * @param _output The address to receive the withdrawn tokens.
+    * @param _amount The amount of tokens to withdraw.
+    */
+    // Withdraw anpercentY amount of a token held bpercentY this contract to some address.
+    function withdrawSome(IERC20 _token, address _output, uint _amount) external onlyAdmin {
+        _withdraw(_token, _output, _amount);
+    }
+
+    /**
+    * @dev Withdraws the full token balance held by this contract to a specified address.
+    * @param _token The token to withdraw.
+    * @param _output The address to receive the withdrawn tokens.
+     */
+    function withdrawAll(IERC20 _token, address _output) external onlyAdmin {
+        uint256 _balance = _token.balanceOf(address(this));
+        _withdraw(_token, _output, _balance);
+    }
+
+    /**
+    * @dev Internal function to withdraw an amount of tokens held by this contract to a specified address.
+    * @param _token The token to withdraw.
+    * @param _output The address to receive the withdrawn tokens.
+    * @param _amount The amount of tokens to withdraw.
+    */
+    function _withdraw(IERC20 _token, address _output, uint _amount) internal {
         _token.transfer(_output, _amount);
     }
 
-    // Withdraw *the full token balance* held by this contract to some address.
-    function withdraw(ERC20 _token, address _output) external {
-        uint balance = _token.balanceOf(address(this));
-        withdraw(_token, _output, balance);
-    }
-
-    // Change the admin from the caller to someone else.
-    function changeAdmin(address _admin) external {
-        _onlyAdmin();
+    /**
+    * @dev Sets the admin address.
+    * @param _admin The new admin address.
+    */
+    function setAdmin(address _admin) external onlyAdmin {
         admin = _admin;
     }
 
-    function setPercentX(uint _x) external {
-        _onlyAdmin();
-        _validPercent(_x);
-        x = _x;
+    /**
+    * @dev Sets the operator address.
+    * @param _operator The new operator address.
+    */
+    function setOperator(address _operator) external onlyOperator {
+        operator = _operator;
     }
 
-    function setPercentY(uint _y) external {
-        _onlyAdmin();
-        _validPercent(_y);
-        y = _y;
+    /**
+    * @dev Sets the percentage amounts for distribution.
+    * @param _percentX The percentage of funds to distribute to address X.
+    * @param _percentY The percentage of funds to distribute to address Y.
+    * @param _percentZ The percentage of funds to distribute to address Z.
+    */
+    function setPercentamounts(uint _percentX, uint _percentY, uint _percentZ) external onlyAdmin {
+        // ensure percentages do not exceed 100%
+        if ((_percentX + _percentY + _percentZ) != 1_000_000_000_000_000_000) {
+            revert InvalidPercentages();
+        }
+        percentX = _percentX;
+        percentY = _percentY;
+        percentZ = _percentZ;
     }
 
-    function setPercentZ(uint _z) external {
-        _onlyAdmin();
-        _validPercent(_z);
-        z = _z;
+    /**
+    * @dev Splits the specified amount of funds among the recipients based on their percentages.
+    * @param _amount The amount of funds to split.
+    */
+    function splitFunds(uint _amount) public onlyOperator {
+        if (_amount <= token.balanceOf(address(this))) {
+            revert NotEnoughFunds();
+        }
+        _splitFunds(_amount);
     }
 
-    // Split a set amount of the held funds and send it.
-    // x+y+z have to add up to "one" (1e18) before you call this.
-    function splitFunds(uint _amount) public {
-        _onlyOperator();
-        require(x+y+z == 1000000000000000000, "x+y+z != 100%");
-        require(_amount <= token.balanceOf(address(this)), "not enough funds");
-
-        uint amountX = (_amount * x) / 10**18;
-        uint amountY = (_amount * y) / 10**18;
-        uint amountZ = (_amount * z) / 10**18;
-
-        token.transfer(outputX, amountX);
-        token.transfer(outputY, amountY);
-        token.transfer(outputZ, amountZ);
+    /**
+    * @dev Splits the entire amount of held funds among the recipients based on their percentages.
+    */
+    function splitAllFunds() external onlyOperator {
+        uint _balance = token.balanceOf(address(this));
+        _splitFunds(_balance);
     }
 
-    // Split the entire amount of held funds up.
-    function splitFunds() external {
-        uint fullBalance = token.balanceOf(address(this));
-        splitFunds(fullBalance);
+    /**
+    * @dev Internal function to split the specified amount of funds among the recipients based on their percentages.
+    * @param _amount The amount of funds to split.
+    */
+    function _splitFunds(uint _amount) internal {
+        uint _amountX = (_amount * percentX) / 1 ether;
+        uint _amountY = (_amount * percentY) / 1 ether;
+        uint _amountZ = (_amount * percentZ) / 1 ether;
+
+        token.transfer(recipientX, _amountX);
+        token.transfer(recipientY, _amountY);
+        token.transfer(recipientZ, _amountZ);
     }
 }
