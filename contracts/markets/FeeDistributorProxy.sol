6d450ca4df3aac3536bb94c85c214c4c505821ab
Zachary Fogg <me@zfo.gg>
2023-05-11 15:54:26 -0400

    added a clear marker about some testing framework values

--- contracts/markets/FeeDistributorProxy.sol
+++ contracts/markets/FeeDistributorProxy.sol
@@ -19,6 +19,7 @@ contract FeeDistributorProxy {
 
     IERC20 public token; // The token to be distributed
 
+    // FIXME: !!!!!testable hardhat addresses -- not real production values yet!!!!!!
     address public immutable recipientX = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; // Address of recipient X
     address public immutable recipientY = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8; // Address of recipient Y
     address public immutable recipientZ = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC; // Address of recipient Z
